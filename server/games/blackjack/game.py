fa8dc02f35e6c4e30f72939f9b17ed82abe587c9
objectinspace <amockery@gmail.com>
2026-02-27 20:38:52 -0500

    Increase blackjack between-hand wait to 200 ticks

--- server/games/blackjack/game.py
+++ server/games/blackjack/game.py
@@ -2142,7 +2142,7 @@ class BlackjackGame(Game):
             self._end_game(remaining[0] if remaining else None)
             return
 
-        self.next_hand_wait_ticks = 100
+        self.next_hand_wait_ticks = 200
         self.rebuild_all_menus()
 
     def _broadcast_settle_result(
